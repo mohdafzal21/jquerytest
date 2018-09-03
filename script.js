504c51e32ad6445d33e00ac6e6336b97fef6bdc6
mohammed afzal <mohammedafzal94@gmail.com>
2018-09-03 17:15:45 +0530

    addClass and removeClass

--- script.js
+++ script.js
@@ -10,9 +10,28 @@ $(document).ready(function(){
 //  window.open("https://www.google.com", "_blank")
 //  });
 
-$('.content p:last-child').on('click', function(){
-    window.open("https://www.facebook.com","_self");
+// $('.content p:last-child').on('click', function(){
+//     window.open("https://www.facebook.com","_self");
+// })
+
+
+// $('li:first').css("background-color" , "yellow")
+
+// $("div:has(p)").addClass('test');
+
+
+
+
+
+
+$("div p").on("click", function(){
+    console.log(this);
+    // $( "div p" ).removeClass( "test" );
+    $(this).addClass('test');
+    
 })
 
 })
+
+
 
